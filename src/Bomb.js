92975265b82573a883ea3b1dd2f828e19cc84b42
zach6585 <zmines@wesleyan.com>
2021-03-04 17:40:35 -0500

    commit tree cc2eb2e587d590edd90eb8e4e665afad0d0f092f

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,12 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends React.component {
+    constructor() {
+        super()
+        this.state = {
+            secondsLeft: 0
+        }
+    }
+    
+}
